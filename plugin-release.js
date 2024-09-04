1537d88ddf2bdc0514081df925d765ddde8fa1cb
marmoure <tinky93winky@gmail.com>
2024-09-04 11:04:46 +0200

    [feature] throw when an error happens

--- plugin-release.js
+++ plugin-release.js
@@ -162,6 +162,10 @@ module.exports = {
 
           this.context.stdout.write(`Released version: ${this.version} OK!\n`);
         }
+
+        async catch(error) {
+          throw error;
+        }
       }
   
       return {
